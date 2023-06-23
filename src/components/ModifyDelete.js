8aadc9e7c37fe7362b84d649ddd8759d6989f8c5
Samuele - <franchisamuele@gmail.com>
2023-06-23 10:39:29 +0200

    Fixed react warning

--- src/components/ModifyDelete.js
+++ src/components/ModifyDelete.js
@@ -5,7 +5,7 @@ export default function ModifyDelete({ id, deletePoi }) {
     <div className="row">
       <div className="col-12 mb-2">
         <Link className="btn btn-warning mb-1" to={"/insertPoi/" + id} role="button">Modifica Poi</Link>{' '}
-        <a className="btn btn-danger mb-1" onClick={deletePoi} role="button">Elimina Poi</a>
+        <a className="btn btn-danger mb-1" href="#" onClick={deletePoi} role="button">Elimina Poi</a>
       </div>
     </div>
   );

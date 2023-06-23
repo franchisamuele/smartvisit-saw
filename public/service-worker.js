33a211d5124f02b917a1346a2e0fe332ae32c411
Samuele - <franchisamuele@gmail.com>
2023-06-23 17:27:33 +0200

    Updated assets

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,31 @@
 const scope = '/';
 
-const CACHE_NAME = 'v2';
+const CACHE_NAME = 'v1';
 const assets = [
   '/',
   '/index.html',
   'images/favicon.ico',
   'images/manifest-icon-192.png',
   'images/manifest-icon-512.png',
-  'images/Photos/torre_di_pisa.png',
-  'images/Photos/cattedrale.png',
   'images/Photos/battistero.png',
+  'images/Photos/camposanto.png',
+  'images/Photos/cattedrale.png',
+  'images/Photos/il_filo_di_mezzogiorno.jpg',
+  'images/Photos/madama_butterfly.jpg',
+  'images/Photos/museo_nazionale.jpg',
+  'images/Photos/navi_antiche.jpg',
+  'images/Photos/oggi_e_gia_domani.jpg',
+  'images/Photos/palazzo_blu.jpeg',
+  'images/Photos/janta_maria_della_spina.jpg',
+  'images/Photos/teatro_san_andrea.jpg',
+  'images/Photos/teatro_verdi.jpg',
+  'images/Photos/torre_di_pisa.jpg',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900',
 ]
 
 self.addEventListener('install', event => {
-  // Wait for cache promise
+  // Wait for cache promise & precache
   event.waitUntil(
     caches
       .open(CACHE_NAME)
@@ -42,6 +52,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches
       .match(event.request) // richiesta già in cache
-      .then(cached => cached || fetch(event.request)) // richiedi
+      .then(cached => cached || fetch(event.request)) // cache || richiedi (va avanti con la fetch normale)
   )
 });

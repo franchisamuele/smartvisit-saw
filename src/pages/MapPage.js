f555a2f519b1fa4b78c4fc2486c07906eb2d608e
Samuele - <franchisamuele@gmail.com>
2023-06-24 08:22:19 +0200

    Map fallback page

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,9 +1,17 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer } from "react-leaflet";
 import Map from '../components/Map'
+import FallbackPage from './FallbackPage'
+import { useEffect, useState } from "react";
 
 export default function MapPage() {
-  return (
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    setIsOnline(navigator.onLine);
+  }, [navigator.onLine]);
+
+  return !isOnline ? <FallbackPage /> : (
     <MapContainer
       style={{ height: "calc(100vh - 56px)" }}
       center={[43.719, 10.401510]}
